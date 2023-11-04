402ad110db23a1a575c820e815be8de2abb4258e
José Augusto <jaugustorc@gmail.com>
2023-11-04 14:43:36 -0300

    parte 1: estruturando o projeto

---
+++ TestProjCalculadora/UnitTest1.cs
@@ -0,0 +1,13 @@
+using ProjCalculadora;
+
+namespace TestProjCalculadora
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Calculadora calc = new();
+        }
+    }
+}
