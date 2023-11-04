af40c41a6525470162658d2540faf6ca72dae444
José Augusto <jaugustorc@gmail.com>
2023-11-04 15:58:56 -0300

    Parte 2: As funções de testes

--- TddCalculadora/Calculadora.cs
+++ TddCalculadora/Calculadora.cs
@@ -8,5 +8,64 @@ namespace ProjCalculadora
 {
     public class Calculadora
     {
+        
+        //public
+        #region public
+        /// <summary>
+        /// Metodo para somar
+        /// </summary>
+        /// <param name="num1">valor 1</param>
+        /// <param name="num2">valor 2</param>
+        /// <returns>valor da soma</returns>
+        public int Somar(int num1,int num2)
+        {
+
+            return 0;
+        }
+        /// <summary>
+        /// Metodo para subtração
+        /// </summary>
+        /// <param name="num1">valor 1</param>
+        /// <param name="num2">valor 2</param>
+        /// <returns>valor da subtração</returns>
+        public int Subtrair(int num1, int num2)
+        {
+
+            return 0;
+        }
+        /// <summary>
+        /// Metodo para multiplicar
+        /// </summary>
+        /// <param name="num1">valor 1</param>
+        /// <param name="num2">valor 2</param>
+        /// <returns>valor da multiplicação</returns>
+        public int Multiplicar(int num1, int num2)
+        {
+
+            return 0;
+        }
+        /// <summary>
+        /// Metodo para dividir
+        /// </summary>
+        /// <param name="num1">valor 1</param>
+        /// <param name="num2">valor 2</param>
+        /// <returns>valor da divição</returns>
+        public int Dividir(int num1, int num2)
+        {
+
+            return 0;
+        }
+        /// <summary>
+        /// Retorna o historico
+        /// </summary>
+        /// <returns></returns>
+        public List<string> Historico()
+        {
+            
+            return new List<string>();
+        }
+        #endregion
+
+
     }
 }
--- TestProjCalculadora/UnitTest1.cs
+++ TestProjCalculadora/UnitTest1.cs
@@ -4,10 +4,100 @@ namespace TestProjCalculadora
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        /// <summary>
+        /// Teste de soma de dois valores
+        /// </summary>
+        /// <param name="num1">valor 1</param>
+        /// <param name="num2">valor 2</param>
+        /// <param name="result">resultado esperado</param>
+        [Theory]
+        [InlineData(1, 1, 2)]
+        [InlineData(4, 53, 57)]
+        public void TestSomar(int num1, int num2, int result)
         {
             Calculadora calc = new();
+
+            int resultado = calc.Somar(num1, num2);
+
+            Assert.Equal(result, resultado);
+        }
+        /// <summary>
+        /// Teste de subtrai de dois valores
+        /// </summary>
+        /// <param name="num1">valor 1</param>
+        /// <param name="num2">valor 2</param>
+        /// <param name="result">resultado esperado</param>
+        [Theory]
+        
+        [InlineData(2, 1, 1)]
+        [InlineData(53, 4, 49)]
+        public void TestSubtrai(int num1, int num2, int result)
+        {
+            Calculadora calc = new();
+
+            int resultado = calc.Subtrair(num1, num2);
+
+            Assert.Equal(result, resultado);
+        }
+        /// <summary>
+        /// Teste de multiplicação de dois valores
+        /// </summary>
+        /// <param name="num1">valor 1</param>
+        /// <param name="num2">valor 2</param>
+        /// <param name="result">resultado esperado</param>
+        [Theory]
+        [InlineData(1, 1, 1)]
+        [InlineData(4, 23, 92)]
+        public void TestMultiplicar(int num1, int num2, int result)
+        {
+            Calculadora calc = new();
+
+            int resultado = calc.Multiplicar(num1, num2);
+
+            Assert.Equal(result, resultado);
+        }
+        /// <summary>
+        /// Teste de divisão de dois valores
+        /// </summary>
+        /// <param name="num1">valor 1</param>
+        /// <param name="num2">valor 2</param>
+        /// <param name="result">resultado esperado</param>
+        [Theory]
+        [InlineData(6, 2, 3)]
+        [InlineData(4, 4, 1)]
+        public void TestDiv(int num1, int num2, int result)
+        {
+            Calculadora calc = new();
+
+            int resultado = calc.Dividir(num1, num2);
+
+            Assert.Equal(result, resultado);
+        }
+        /// <summary>
+        /// Testa divisão por zero
+        /// </summary>
+        [Fact]
+        public void TestarDivisaoPorZero()
+        {
+            Calculadora calc = new();
+
+            Assert.Throws<DivideByZeroException>(()=>calc.Dividir(3,0));
+        }
+
+        [Fact]
+        public void TestarHistorico()
+        {
+            Calculadora calc = new();
+
+            calc.Somar(1, 2);
+            calc.Somar(1, 2);
+            calc.Somar(1, 2);
+            calc.Somar(1, 2);
+
+            var lista = calc.Historico();
+
+            Assert.NotEmpty(lista);
+            Assert.Equal(3, lista.Count);
         }
     }
 }
